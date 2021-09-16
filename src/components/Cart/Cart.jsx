0fa674ab2b35341ff50101bc20f76e098b9c7c2e
Dan St. Aubin <staubindj@gmail.com>
2021-09-15 21:30:21 -0500

    added sumIngredients to prepare our grocery list

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,25 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import RecipeCard from '../RecipeCard/RecipeCard';
+import { Tabs, Tab, Container } from "react-bootstrap";
+
+// this gathers the ingredients for the recipe display
+function sumIngredients(recipes) {
+    let totalIngredients = {};
+    // need recipe list 
+    // also need servings per recipe
+    for (recipe of recipes) {
+        for (ingredient of recipe.extendedIngredients) {
+                if (totalIngredients[ingredient.aisle] === undefined) {
+                    totalIngredients[ingredient.aisle] = new Set();
+                    totalIngredients[ingredient.aisle].add(ingredient.name)
+                } else {
+                    totalIngredients[ingredient.aisle].add(ingredient.name)
+                }
+            }
+        }
+    return totalIngredients
+}
 
 function Cart() {
     const dispatch = useDispatch();
@@ -16,11 +35,18 @@ function Cart() {
 
     // display the list of recipe cards in return
     return (
-        <>
-            {cart.map(recipe => {
-                return <RecipeCard key={recipe.id} recipe={recipe}/>
-            })}       
-        </>
+        <Container>
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+                <Tab eventKey="home" title="Recipes">
+                    {cart.map(recipe => {
+                        return <RecipeCard key={recipe.id} recipe={recipe}/>
+                    })}
+                </Tab>
+                <Tab eventKey="profile" title="hai">
+                    <p>hi</p>
+                </Tab>
+            </Tabs>        
+        </Container>
     );
 }
 
